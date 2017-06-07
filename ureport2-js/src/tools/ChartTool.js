5363a3849488ffa62b2d36e9d699bf8261a63783
jacky6024 <jacky6024@sina.com>
2017-06-07 16:31:48 +0800

    添加Chart工具栏

--- ureport2-js/src/tools/ChartTool.js
+++ ureport2-js/src/tools/ChartTool.js
@@ -10,7 +10,7 @@ export default class ChartTool extends Tool{
     buildButton(){
         const group=$(`<div class="btn-group"></div>`);
         const mainBtn=$(`<button type="button" class="btn btn-default dropdown-toggle" style="border:none;border-radius:0;background: #f8f8f8;padding: 6px 5px;" data-toggle="dropdown" title="图表">
-            <i class="ureport ureport-bar" style="color: #0e90d2;"></i>
+            <i class="ureport ureport-pie" style="color: #0e90d2;"></i>
             <span class="caret"></span>
         </button>`);
         const ul=$(`<ul class="dropdown-menu" role="menu"></ul>`);
@@ -32,12 +32,36 @@ export default class ChartTool extends Tool{
                 </a>
             </li>`);
         ul.append(bar);
+        const horBar=$(`<li>
+                <a href="###">
+                    <i class="ureport ureport-horizontal-bar" style="color: #0e90d2;"></i> 水平柱状图
+                </a>
+            </li>`);
+        ul.append(horBar);
+        const area=$(`<li>
+                <a href="###">
+                    <i class="ureport ureport-area" style="color: #0e90d2;"></i> 面积图
+                </a>
+            </li>`);
+        ul.append(area);
         const radar=$(`<li>
                 <a href="###">
                     <i class="ureport ureport-radar" style="color: #0e90d2;"></i> 雷达图
                 </a>
             </li>`);
         ul.append(radar);
+        const polar=$(`<li>
+                <a href="###">
+                    <i class="ureport ureport-polar" style="color: #0e90d2;"></i> 极坐标图
+                </a>
+            </li>`);
+        ul.append(polar);
+        const scatter=$(`<li>
+                <a href="###">
+                    <i class="ureport ureport-scatter" style="color: #0e90d2;"></i> 散点图
+                </a>
+            </li>`);
+        ul.append(scatter);
         const bubble=$(`<li>
                 <a href="###">
                     <i class="ureport ureport-bubble" style="color: #0e90d2;"></i> 气泡图
