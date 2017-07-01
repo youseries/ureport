9a78a1830b60cec0de2a81901433688a57929000
jacky6024 <jacky6024@sina.com>
2017-07-01 15:27:58 +0800

    在SQL数据集配置窗口增加当前数据源下表和视图罗列功能，通过双击表名即可完成SQL添加

--- ureport2-console/src/main/java/com/bstek/ureport/console/designer/DatasourceServletAction.java
+++ ureport2-console/src/main/java/com/bstek/ureport/console/designer/DatasourceServletAction.java
@@ -19,6 +19,7 @@ import java.beans.PropertyDescriptor;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -129,6 +130,42 @@ public class DatasourceServletAction extends RenderPageServletAction {
 			writeObjectToJson(resp, result);
 		}catch(Exception ex){
 			throw new ReportDesignException(ex);
+		}
+	}
+	
+	public void buildDatabaseTables(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Connection conn=null;
+		ResultSet rs = null;
+		try{
+			conn=buildConnection(req);
+			DatabaseMetaData metaData = conn.getMetaData();
+			String url = metaData.getURL();
+			String schema = null;
+			if (url.toLowerCase().contains("oracle")) {
+				schema = metaData.getUserName();
+			}
+			List<Map<String,String>> tables = new ArrayList<Map<String,String>>();
+			rs = metaData.getTables(null, schema, "%", new String[] { "TABLE","VIEW" });
+			while (rs.next()) {
+				Map<String,String> table = new HashMap<String,String>();
+				table.put("name",rs.getString("TABLE_NAME"));
+				table.put("type",rs.getString("TABLE_TYPE"));
+				tables.add(table);
+			}
+			writeObjectToJson(resp, tables);
+		}catch(Exception ex){
+			throw new ServletException(ex);
+		}finally{
+			try {
+				if(rs!=null){
+					rs.close();
+				}
+				if(conn!=null){
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
