fca6f0677f138e143d5880bbd8fa5c6109bec9fd
jacky6024 <jacky6024@sina.com>
2017-08-28 09:04:12 +0800

    完善Report对象的临时存储

--- ureport2-console/src/main/java/com/bstek/ureport/console/cache/ReportMapObject.java
+++ ureport2-console/src/main/java/com/bstek/ureport/console/cache/ReportMapObject.java
@@ -51,6 +51,7 @@ public class ReportMapObject {
 		this.reportMap.put(file, report);
 	}
 	public Report getReport(String file){
+		this.start=System.currentTimeMillis();
 		return this.reportMap.get(file);
 	}
 	public boolean isExpired(){
