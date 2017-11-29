5cc401b9aec36825d4417de1104b9bf7c9827c06
jacky6024 <jacky6024@sina.com>
2017-11-29 14:30:24 +0800

    修复通过#表达式取当前单元格值，而当前单元格又配置数据映射情况下，取到的仍然是实际值，而非映射值的BUG

--- ureport2-core/src/main/java/com/bstek/ureport/expression/model/expr/CurrentCellValueExpression.java
+++ ureport2-core/src/main/java/com/bstek/ureport/expression/model/expr/CurrentCellValueExpression.java
@@ -29,6 +29,6 @@ public class CurrentCellValueExpression extends BaseExpression {
 
 	@Override
 	protected ExpressionData<?> compute(Cell cell, Cell currentCell,Context context) {
-		return new ObjectExpressionData(cell.getData());
+		return new ObjectExpressionData(cell.getFormatData());
 	}
 }
