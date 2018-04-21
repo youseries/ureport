a949fe22d08edf7676ba9753a4e7dde997590120
Yin Guo Wei <yinguowei@gmail.com>
2018-04-21 08:49:40 +0800

    fix Nullpoint exception
    NullPointException when set :  ureport.disableHttpSessionReportCache=true

--- ureport2-core/src/main/java/com/bstek/ureport/cache/CacheUtils.java
+++ ureport2-core/src/main/java/com/bstek/ureport/cache/CacheUtils.java
@@ -48,7 +48,9 @@ public class CacheUtils implements ApplicationContextAware{
 	
 	public static void storeChartDataMap(Map<String, ChartData> map){
 		String key=CHART_DATA_key;
-		reportCache.storeObject(key, map);
+		if(reportCache!=null){
+		    reportCache.storeObject(key, map);
+		}
 	}
 	
 	public static Object getObject(String file){
